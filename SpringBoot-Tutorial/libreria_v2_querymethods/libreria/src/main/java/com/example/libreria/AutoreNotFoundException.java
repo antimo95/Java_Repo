63c47bb5ec18e4f626a30b2525902cef6559dfc7
Antimo Barbato <161333298+antimo95@users.noreply.github.com>
2026-06-10 08:42:29 +0200

    Delete SpringBoot-Tutorial/libreria_v2_querymethods/libreria directory

--- SpringBoot-Tutorial/libreria_v2_querymethods/libreria/src/main/java/com/example/libreria/AutoreNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.libreria;
-
-public class AutoreNotFoundException extends RuntimeException {
-    public AutoreNotFoundException(Long id) {
-
-        super("Autore con id" + id + "non trovato");
-    }
-}
--- SpringBoot-Tutorial/libreria_v2_querymethods/libreria/src/main/java/com/example/libreria/LibroNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.libreria;
-
-public class LibroNotFoundException extends RuntimeException {
-    public LibroNotFoundException(Long id) {
-        super("Libro con id "+ id + "non trovato");
-    }
-}
